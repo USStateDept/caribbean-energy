535cba15d7fb855156eede2296aa2e8e2558ccfa
brodydittemore-eDip <dittemoremb@state.com>
2014-04-24 15:43:31 -0400

    Updated SQL join for Latin America only

--- sql/Petro_Caribe_Analysis_min_50m_Join.sql
+++ sql/Petro_Caribe_Analysis_min_50m_Join.sql
@@ -22,4 +22,36 @@ SELECT
 	"Petro_Caribe_Analysis_min"."Cell_2",
 	the_geom
 FROM opengeo."ne_50m_admin_0_countries_lakes" LEFT OUTER JOIN public."Petro_Caribe_Analysis_min" ON (ne_50m_admin_0_countries_lakes.sovereignt = "Petro_Caribe_Analysis_min"."Country")
-WHERE "Petro_Caribe_Analysis_min"."Country" is not null;
+WHERE
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Argentina' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Barbados' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Belize' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Chile' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Colombia' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Costa Rica' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Cuba' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Dominca' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Dominican Republic' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Ecuador' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'El Salvador' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'France' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Grenada' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Haiti' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Honduras' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Jamaica' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Mexico' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Netherlands' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Nicaragua' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Panama' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Paraguay' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Peru' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Saint Kitts and Nevis' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Saint Lucia' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Saint Vincent and the Grenadines' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Suriname' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'The Bahamas' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Trinidad and Tobago' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'United Kingdom' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Uruguay' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'Venezuela' OR
+	ne_50m_admin_0_countries_lakes.sovereignt = 'United States of America';
