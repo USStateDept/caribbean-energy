2014c642727e16a791c12442b7cc8a5e3bfc30a3
brodydittemore-eDip <dittemoremb@state.com>
2014-05-01 18:48:40 -0400

    Adding ieCheck

---
+++ js/ieCheck.js
@@ -0,0 +1,6 @@
+function ieCheck (){
+	if (navigator.appName == "Microsoft Internet Explorer")
+		{alert("This functionality is only available with a modern web browser, such as Chrome. We encourage you to use it.");}
+	else if (navigator.userAgent.indexOf("MSIE") > -1)
+		{alert("his functionality is only available with a modern web browser, such as Chrome. We encourage you to use it");}
+}
